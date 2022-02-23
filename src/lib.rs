98923cc65f1bb83dd0f6438d8e7819676b3c8bc4
Rod Conan <rconan@gmto.org>
2022-02-23 14:29:48 -0800

    ADD SAVING INTERFACE

--- src/lib.rs
+++ src/lib.rs
@@ -7,49 +7,59 @@ MATLAB MAT file I/O C library
 ## Examples
 Loading a mat file
 ```
-use matio_rs::Loader;
-let mat_file = Loader::new("data.mat").load()?;
+use matio_rs::{MatFile, Loading};
+let mat_file = MatFile::load("data.mat")?;
 # Ok::<(), matio_rs::MatioError>(())
 ```
 Reading a scalar Matlab variable: a = π
 ```
-# use matio_rs::Loader;
-# let mat_file = Loader::new("data.mat").load()?;
+# use matio_rs::{MatFile, Loading};
+# let mat_file = MatFile::load("data.mat")?;
 if let Ok(mat) = mat_file.read("a") {
     println!("{mat}");
-    let a: Option<f64> = mat.into();
+    let a: f64 = mat.into();
     println!("{a:?}");
 }
 # Ok::<(), matio_rs::MatioError>(())
 ```
 Reading a Matlab vector: b = [3.0, 1.0, 4.0, 1.0, 6.0]
 ```
-# use matio_rs::Loader;
-# let mat_file = Loader::new("data.mat").load()?;
+# use matio_rs::{MatFile, Loading};
+# let mat_file = MatFile::load("data.mat")?;
 if let Ok(mat) = mat_file.read("b") {
     println!("{mat}");
-    let b: Option<Vec<f64>> = mat.into();
+    let b: Vec<f64> = mat.into();
     println!("{b:?}");
 }
 # Ok::<(), matio_rs::MatioError>(())
 ```
 Reading a Matlab array: c = [4, 2; 3, 7]
 ```
-# use matio_rs::Loader;
-# let mat_file = Loader::new("data.mat").load()?;
+# use matio_rs::{MatFile, Loading};
+# let mat_file = MatFile::load("data.mat")?;
 if let Ok(mat) = mat_file.read("c") {
     println!("{mat}");
-    let c: Option<Vec<f64>> = mat.into();
+    let c: Vec<f64> = mat.into();
     println!("{c:?}");
 }
 # Ok::<(), matio_rs::MatioError>(())
 ```
+Saving to a mat file
+```
+use matio_rs::{MatFile, MatVar, Saving};
+let mat_file = MatFile::save("data.rs.mat")?;
+mat_file.write(MatVar::<f64>::new("a", 2f64.sqrt())?);
+let b = (0..5).map(|x| (x as f64).cosh()).collect::<Vec<f64>>();
+mat_file.write(MatVar::<Vec<f64>>::new("b", b)?);
+# Ok::<(), matio_rs::MatioError>(())
+```
 */
 
-use ffi;
 use std::{
+    any::{type_name, TypeId},
     fmt::Display,
     fs, io,
+    marker::PhantomData,
     path::{Path, PathBuf},
     ptr,
 };
@@ -65,6 +75,10 @@ pub enum MatioError {
     MatName(#[from] std::ffi::NulError),
     #[error("reading mat var {0} failed")]
     MatVarRead(String),
+    #[error("creating mat var {0} failed")]
+    MatVarCreate(String),
+    #[error("Rust ({0}) and Matlab ({1}) types do not match")]
+    MatType(String, String),
 }
 pub type Result<T> = std::result::Result<T, MatioError>;
 
@@ -74,12 +88,12 @@ pub enum AccessMode {
     ReadWrite,
 }
 
-/// Mat file loader
-pub struct Loader {
+/// Mat file builder
+pub struct Builder {
     mat_name: PathBuf,
     access_mode: AccessMode,
 }
-impl Loader {
+impl Builder {
     /// Creates a new mat file loader object from the `path`
     pub fn new<P: AsRef<Path>>(path: P) -> Self {
         Self {
@@ -121,6 +135,18 @@ impl Loader {
             )))
         }
     }
+    pub fn save(self) -> Result<MatFile> {
+        let mat_name = std::ffi::CString::new(self.mat_name.to_str().unwrap())?;
+        let mat_t =
+            unsafe { ffi::Mat_CreateVer(mat_name.as_ptr(), ptr::null(), ffi::mat_ft_MAT_FT_MAT5) };
+        if mat_t.is_null() {
+            Err(MatioError::MatOpen(
+                self.mat_name.to_str().unwrap().to_string(),
+            ))
+        } else {
+            Ok(MatFile { mat_t })
+        }
+    }
 }
 
 /// Mat file
@@ -134,72 +160,179 @@ impl Drop for MatFile {
         }
     }
 }
-impl MatFile {
+/// Mat file loading interface
+pub trait Loading {
+    /// Loads a mat file from `path`
+    fn load<P: AsRef<Path>>(path: P) -> Result<Self>
+    where
+        Self: Sized;
     /// Reads a variable `name` from the mat file
-    pub fn read<S: Into<String>>(&self, name: S) -> Result<MatVar> {
+    fn read<T: 'static, S: Into<String>>(&self, name: S) -> Result<MatVar<T>>;
+}
+impl Loading for MatFile {
+    fn load<P: AsRef<Path>>(path: P) -> Result<Self> {
+        Builder::new(path).load()
+    }
+    fn read<T: 'static, S: Into<String>>(&self, name: S) -> Result<MatVar<T>> {
         let c_name = std::ffi::CString::new(name.into())?;
         let matvar_t = unsafe { ffi::Mat_VarRead(self.mat_t, c_name.as_ptr()) };
         if matvar_t.is_null() {
             Err(MatioError::MatVarRead(c_name.to_str().unwrap().to_string()))
         } else {
-            Ok(MatVar { matvar_t })
+            MatVar {
+                matvar_t,
+                data_type: PhantomData,
+            }
+            .match_types()
+        }
+    }
+}
+/// Mat file saving interface
+pub trait Saving {
+    /// saves a mat file to `path`
+    fn save<P: AsRef<Path>>(path: P) -> Result<Self>
+    where
+        Self: Sized;
+    /// Writes a Matlab variable to the mat file
+    fn write<T>(&self, mat_var: MatVar<T>);
+}
+impl Saving for MatFile {
+    fn save<P: AsRef<Path>>(path: P) -> Result<Self> {
+        Builder::new(path).save()
+    }
+    fn write<T>(&self, mat_var: MatVar<T>) {
+        unsafe {
+            ffi::Mat_VarWrite(
+                self.mat_t,
+                mat_var.matvar_t,
+                ffi::matio_compression_MAT_COMPRESSION_NONE,
+            );
         }
     }
 }
 
 /// Matlab variable
-pub struct MatVar {
+pub struct MatVar<T> {
     matvar_t: *mut ffi::matvar_t,
+    data_type: PhantomData<T>,
 }
-impl Drop for MatVar {
+impl<T> Drop for MatVar<T> {
     fn drop(&mut self) {
         unsafe {
             ffi::Mat_VarFree(self.matvar_t);
         }
     }
 }
-impl Display for MatVar {
+impl<T> Display for MatVar<T> {
     fn fmt(&self, _: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         unsafe { ffi::Mat_VarPrint(self.matvar_t, 0) }
         Ok(())
     }
 }
-
-impl From<MatVar> for Option<f64> {
-    fn from(mat_var: MatVar) -> Self {
+impl<T: 'static> MatVar<T> {
+    /// Checks Rust and Matlab types compatibility
+    pub fn match_types(self) -> Result<Self> {
         unsafe {
-            if (*mat_var.matvar_t).data_type == 9 {
-                Some(((*mat_var.matvar_t).data as *mut f64).read())
-            } else {
-                println!("The Matlab var type do not match the expect Rust type");
-                None
+            if (TypeId::of::<T>() == TypeId::of::<f64>()
+                || TypeId::of::<T>() == TypeId::of::<Vec<f64>>())
+                && (*self.matvar_t).data_type == ffi::matio_types_MAT_T_DOUBLE
+            {
+                return Ok(self);
             }
+            Err(MatioError::MatType(
+                type_name::<T>().to_string(),
+                {
+                    match (*self.matvar_t).data_type {
+                        ffi::matio_types_MAT_T_DOUBLE => "DOUBLE",
+                        _ => "UNKNOWN",
+                    }
+                }
+                .to_string(),
+            ))
         }
     }
 }
 
-impl From<MatVar> for Option<Vec<f64>> {
-    fn from(mat_var: MatVar) -> Self {
-        unsafe {
-            if (*mat_var.matvar_t).data_type == 9 {
-                let rank = (*mat_var.matvar_t).rank as usize;
-                let mut dims: Vec<u64> = Vec::with_capacity(rank);
-                ptr::copy((*mat_var.matvar_t).dims, dims.as_mut_ptr(), rank);
-                dims.set_len(rank);
-                let length = dims.into_iter().product::<u64>() as usize;
+impl MatVar<f64> {
+    /// Creates a new Matlab variable `name`
+    pub fn new<S: Into<String>>(name: S, mut data: f64) -> Result<Self> {
+        let c_name = std::ffi::CString::new(name.into())?;
+        let mut dims = [1, 1];
+        let matvar_t = unsafe {
+            ffi::Mat_VarCreate(
+                c_name.as_ptr(),
+                ffi::matio_classes_MAT_C_DOUBLE,
+                ffi::matio_types_MAT_T_DOUBLE,
+                2,
+                dims.as_mut_ptr(),
+                &mut data as *mut _ as *mut std::ffi::c_void,
+                0,
+            )
+        };
+        if matvar_t.is_null() {
+            Err(MatioError::MatVarCreate(
+                c_name.to_str().unwrap().to_string(),
+            ))
+        } else {
+            Ok(MatVar {
+                matvar_t,
+                data_type: PhantomData,
+            })
+        }
+    }
+}
+impl MatVar<Vec<f64>> {
+    /// Creates a new Matlab variable `name`
+    pub fn new<S: Into<String>>(name: S, mut data: Vec<f64>) -> Result<Self> {
+        let c_name = std::ffi::CString::new(name.into())?;
+        let mut dims = [1, data.len() as u64];
+        let matvar_t = unsafe {
+            ffi::Mat_VarCreate(
+                c_name.as_ptr(),
+                ffi::matio_classes_MAT_C_DOUBLE,
+                ffi::matio_types_MAT_T_DOUBLE,
+                2,
+                dims.as_mut_ptr(),
+                data.as_mut_ptr() as *mut std::ffi::c_void,
+                0,
+            )
+        };
+        if matvar_t.is_null() {
+            Err(MatioError::MatVarCreate(
+                c_name.to_str().unwrap().to_string(),
+            ))
+        } else {
+            Ok(MatVar {
+                matvar_t,
+                data_type: PhantomData,
+            })
+        }
+    }
+}
 
-                let mut value: Vec<f64> = Vec::with_capacity(length);
-                ptr::copy(
-                    (*mat_var.matvar_t).data as *mut f64,
-                    value.as_mut_ptr(),
-                    length,
-                );
-                value.set_len(length);
-                Some(value)
-            } else {
-                println!("The Matlab var type do not match the expect Rust type");
-                None
-            }
+impl From<MatVar<f64>> for f64 {
+    fn from(mat_var: MatVar<f64>) -> Self {
+        unsafe { ((*mat_var.matvar_t).data as *mut f64).read() }
+    }
+}
+
+impl From<MatVar<Vec<f64>>> for Vec<f64> {
+    fn from(mat_var: MatVar<Vec<f64>>) -> Self {
+        unsafe {
+            let rank = (*mat_var.matvar_t).rank as usize;
+            let mut dims: Vec<u64> = Vec::with_capacity(rank);
+            ptr::copy((*mat_var.matvar_t).dims, dims.as_mut_ptr(), rank);
+            dims.set_len(rank);
+            let length = dims.into_iter().product::<u64>() as usize;
+
+            let mut value: Vec<f64> = Vec::with_capacity(length);
+            ptr::copy(
+                (*mat_var.matvar_t).data as *mut f64,
+                value.as_mut_ptr(),
+                length,
+            );
+            value.set_len(length);
+            value
         }
     }
 }
@@ -209,37 +342,48 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_loader() {
-        let _mat_file = Loader::new("data.mat").load().unwrap();
+    fn test_load() {
+        let _mat_file = MatFile::load("data.mat").unwrap();
     }
 
     #[test]
     fn test_read_scalar() {
-        let mat_file = Loader::new("data.mat").load().unwrap();
-        if let Ok(mat) = mat_file.read("a") {
-            println!("{mat}");
-            let a: Option<f64> = mat.into();
-            println!("{a:?}");
-        }
+        let mat_file = MatFile::load("data.mat").unwrap();
+        let mat = mat_file.read("a").unwrap();
+        let a: f64 = mat.into();
+        assert_eq!(a, std::f64::consts::PI);
     }
 
     #[test]
     fn test_read_1d() {
-        let mat_file = Loader::new("data.mat").load().unwrap();
-        if let Ok(mat) = mat_file.read("b") {
-            println!("{mat}");
-            let b: Option<Vec<f64>> = mat.into();
-            println!("{b:?}");
-        }
+        let mat_file = MatFile::load("data.mat").unwrap();
+        let mat = mat_file.read("b").unwrap();
+        let b: Vec<f64> = mat.into();
+        assert_eq!(b, vec![3f64, 1., 4., 1., 6.])
     }
 
     #[test]
     fn test_read_2d() {
-        let mat_file = Loader::new("data.mat").load().unwrap();
-        if let Ok(mat) = mat_file.read("c") {
-            println!("{mat}");
-            let c: Option<Vec<f64>> = mat.into();
-            println!("{c:?}");
+        let mat_file = MatFile::load("data.mat").unwrap();
+        let mat = mat_file.read("c").unwrap();
+        let c: Vec<f64> = mat.into();
+        assert_eq!(c, vec![4f64, 3., 2., 7.])
+    }
+
+    #[test]
+    fn test_save() {
+        let b = (0..5).map(|x| (x as f64).cosh()).collect::<Vec<f64>>();
+        {
+            let mat_file = MatFile::save("data.rs.mat").unwrap();
+            mat_file.write(MatVar::<f64>::new("a", 2f64.sqrt()).unwrap());
+            mat_file.write(MatVar::<Vec<f64>>::new("b", b.clone()).unwrap());
         }
+        let mat_file = MatFile::load("data.rs.mat").unwrap();
+        let mat = mat_file.read("a").unwrap();
+        let a: f64 = mat.into();
+        assert_eq!(a, 2f64.sqrt());
+        let mat = mat_file.read("b").unwrap();
+        let bb: Vec<f64> = mat.into();
+        assert_eq!(b, bb);
     }
 }
