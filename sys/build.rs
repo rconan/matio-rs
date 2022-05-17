3cd107359416567948048024791439df629bcf06
Rod Conan <rconan@gmto.org>
2022-05-17 16:28:28 -0700

    DOCS_RS ENV VAR TO DISABLE BUILD IN DOCS.RS

--- sys/build.rs
+++ sys/build.rs
@@ -1,16 +1,18 @@
 use std::path::Path;
 
 fn main() {
-    println!("cargo:rustc-link-lib=matio");
-    println!("cargo:rerun-if-changed=wrapper.h");
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
+    if std::env::var("DOCS_RS").is_err() {
+        println!("cargo:rustc-link-lib=matio");
+        println!("cargo:rerun-if-changed=wrapper.h");
+        let bindings = bindgen::Builder::default()
+            .header("wrapper.h")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings");
 
-    let out_path = Path::new("src");
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        let out_path = Path::new("src");
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+    }
 }
