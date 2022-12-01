02a35edd048b58726f933fc417e8a2de8f9e4412
Rod Conan <rconan@gmto.org>
2022-12-01 09:17:37 -0800

    EXAMPLE IMPL MAYBES

--- examples/bin2mat.rs
+++ examples/bin2mat.rs
@@ -2,7 +2,7 @@
 
 use std::fs::File;
 
-use matio_rs::{Mat, MatFile, MayBeFrom};
+use matio_rs::{Mat, MatFile, MayBeFrom, MayBeInto};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Default)]
@@ -11,15 +11,36 @@ pub struct Opds {
     pub mask: Vec<bool>,
 }
 
+impl<'a> MayBeFrom<'a, &'a Opds> for Mat<'a> {
+    fn maybe_from<S: Into<String>>(name: S, data: &'a Opds) -> matio_rs::Result<Self> {
+        let m: Vec<u8> = data
+            .mask
+            .iter()
+            .map(|&m| if m { 1u8 } else { 0u8 })
+            .collect();
+        let mats = vec![
+            Mat::maybe_from("values", &data.values)?,
+            Mat::maybe_from("mask", m)?,
+        ];
+        MayBeFrom::maybe_from(name, mats)
+    }
+}
+
+impl<'a> MayBeInto<Opds> for Mat<'a> {
+    fn maybe_into(self) -> matio_rs::Result<Opds> {
+        let m: Vec<u8> = self.field("values")?.get(0).unwrap().maybe_into()?;
+        Ok(Opds {
+            values: self.field("values")?.get(0).unwrap().maybe_into()?,
+            mask: m
+                .into_iter()
+                .map(|m| if m == 1 { true } else { false })
+                .collect(),
+        })
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let data: Opds = bincode::deserialize_from(File::open("data/opds.bin")?)?;
-    let mask = data.mask.into_iter().map(|x| x as u8).collect::<Vec<u8>>();
-    let mat_data = vec![
-        Mat::maybe_from("values", &data.values)?,
-        Mat::maybe_from("mask", &mask)?,
-    ];
-    let mat_struct = Mat::maybe_from("opd", mat_data)?;
-    let mat_file = MatFile::save("data/opds.mat")?;
-    mat_file.write(mat_struct);
+    MatFile::save("data/opds.mat")?.var("opds", &data)?;
     Ok(())
 }
