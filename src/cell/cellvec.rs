507f087c51c7ffa295f72773dc8f38ea12b29546
Rod Conan <rconan@gmto.org>
2025-12-15 08:43:17 -0800

    CellVec ::into_vec , ::from_vec & tests

--- src/cell/cellvec.rs
+++ src/cell/cellvec.rs
@@ -4,8 +4,8 @@ use super::{Cell, CellBounds, LastCell};
 
 pub trait CellVec<T>: CellBounds<Item = T> {
     fn get(&self, idx: usize) -> Option<&T>;
-    fn into(self) -> Vec<T>;
-    fn from(v: &mut Vec<T>) -> Option<Self>
+    fn into_vec(self) -> Vec<T>;
+    fn from_vec(v: &mut Vec<T>) -> Option<Self>
     where
         Self: Sized;
 }
@@ -19,11 +19,11 @@ impl<T> CellVec<T> for LastCell<T> {
         }
     }
 
-    fn into(self) -> Vec<T> {
+    fn into_vec(self) -> Vec<T> {
         vec![self.item]
     }
 
-    fn from(v: &mut Vec<T>) -> Option<Self> {
+    fn from_vec(v: &mut Vec<T>) -> Option<Self> {
         if let Some(item) = v.pop() {
             Some(Self { item })
         } else {
@@ -44,16 +44,16 @@ where
         }
     }
 
-    fn into(self) -> Vec<T> {
+    fn into_vec(self) -> Vec<T> {
         let mut v = vec![self.item];
-        let nv = <C as CellVec<T>>::into(self.next_cell);
+        let nv = <C as CellVec<T>>::into_vec(self.next_cell);
         v.extend(nv);
         v
     }
 
-    fn from(v: &mut Vec<T>) -> Option<Self> {
+    fn from_vec(v: &mut Vec<T>) -> Option<Self> {
         let Some(item) = v.pop() else { return None };
-        if let Some(next_cell) = <C as CellVec<T>>::from(v) {
+        if let Some(next_cell) = <C as CellVec<T>>::from_vec(v) {
             Some(Self { item, next_cell })
         } else {
             None
@@ -66,7 +66,7 @@ where
     C: CellVec<T>,
 {
     fn from(cell: Cell<T, C>) -> Self {
-        <Cell<T, C> as CellVec<T>>::into(cell)
+        <Cell<T, C> as CellVec<T>>::into_vec(cell)
     }
 }
 
@@ -88,7 +88,7 @@ where
     C: CellVec<T>,
 {
     fn from(mut v: Vec<T>) -> Self {
-        <Self as CellVec<T>>::from(&mut v).unwrap()
+        <Self as CellVec<T>>::from_vec(&mut v).unwrap()
     }
 }
 
@@ -98,6 +98,24 @@ where
 {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
         let mut v: Vec<T> = iter.into_iter().collect();
-        <Self as CellVec<T>>::from(&mut v).unwrap()
+        <Self as CellVec<T>>::from_vec(&mut v).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // #[test]
+    // fn from_vec() {
+    //     let v: Vec<_> = (0i32..5).collect();
+    //     let c: Cell<_, Cell<_,_>> = Cell::<i32, _>::from(v);
+    // }
+
+    #[test]
+    fn into_vec() {
+        let c = Cell::new(0i32).push(1i32).push(2i32).push(3i32).push(4i32);
+        let v: Vec<i32> = c.into();
+        dbg!(&v);
     }
 }
